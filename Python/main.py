d5c3c9b7417c44781d1cad67a1899be6473aab37
yfsanchez <yfsanchez@gmail.com>
2022-01-28 19:43:34 -0300

    Python code example objetos

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,11 @@
 from unicodedata import name
 
+from car import Car
+
 
 if __name__ == "__main__":
     print("hola...ok")
+    car = Car()
+    car.license = "HJFGHG"
+    car.driver = "GHkdjf HJKdf"
+    print(vars(car))
