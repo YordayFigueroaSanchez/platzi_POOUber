0a2758811184e9bc3ec99d21a1e4305854a805b3
yfsanchez <yfsanchez@gmail.com>
2022-01-28 20:14:05 -0300

    JavaScript constructor example

--- JS/Car.js
+++ JS/Car.js
@@ -1,6 +1,12 @@
-function Car() {
+function Car(licence, driver) {
     this.id;
-    this.license;
-    this.driver;
+    this.license = licence;
+    this.driver = driver;
     this.passenger;
+}
+
+Car.prototype.printCar = function () {
+    console.log(this.driver);
+    console.log(this.driver.name);
+    console.log(this.driver.document);
 }
