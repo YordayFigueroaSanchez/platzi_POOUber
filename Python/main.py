0dffdaa21ab132cb63eb73539aece7397047bc68
yfsanchez <yfsanchez@gmail.com>
2022-01-29 00:44:13 -0300

    Python Herencia example

--- Python/main.py
+++ Python/main.py
@@ -2,9 +2,13 @@ from unicodedata import name
 from account import Acoount
 
 from car import Car
+from uberx import Uberx
 
 
 if __name__ == "__main__":
     print("hola...ok")
     car = Car("HJUY-3", Acoount("Ohyjuks","12345-90"))
     print(vars(car))
+
+    carUberX = Uberx("HJUY-3", Acoount("Ohyjuks","12345-90"),"kjdk","rklfklf")
+    print(vars(carUberX))
