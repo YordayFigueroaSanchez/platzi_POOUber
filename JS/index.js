b9e96e615197e2d377fc8badb0fac7d06d818af7
yfsanchez <yfsanchez@gmail.com>
2022-01-29 00:57:29 -0300

    JavaScript Herencia example

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,11 @@ var car = new Car(
     )
 
 car.printCar()
+
+var carUberBlack = new UberBlack(
+    "DFDF", 
+    new Account("Faresdujne", "178345-2"),
+    ["sdd","sdfdf"],
+    [1,2,3,4,5]
+    )
+carUberBlack.printCar()
