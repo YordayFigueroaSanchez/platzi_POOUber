ef3641142fea02b32e3d882ec2eee4ff4fd43d7e
yfsanchez <yfsanchez@gmail.com>
2022-01-28 23:13:01 -0300

    Python constructor example

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,12 @@
+from account import Acoount
+
+
 class Car:
     id          = int
     license     = str
-    driver      = str
+    driver      = Acoount("","")
     passagenger = str
+
+    def __init__(self, license, driver):
+        self.license    = license
+        self.driver     = driver
--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,10 @@
 from unicodedata import name
+from account import Acoount
 
 from car import Car
 
 
 if __name__ == "__main__":
     print("hola...ok")
-    car = Car()
-    car.license = "HJFGHG"
-    car.driver = "GHkdjf HJKdf"
+    car = Car("HJUY-3", Acoount("Ohyjuks","12345-90"))
     print(vars(car))
